e66b79d1b372e62d0e715b8564fc3edd850edc04
WinsonL28 <winsonliang@live.com>
2024-03-31 21:06:46 -0700

    add redirect to next all main links

--- scripts/main.js
+++ scripts/main.js
@@ -57,6 +57,18 @@ function redirect() {
     document.getElementById("workout-redirect").onclick = function () {
         location.href = "each_workout.html";
     };
+
+    document.getElementById("food-redirect").onclick = function () {
+        location.href = "diet_recomendation.html";
+    };
+
+    document.getElementById("physical_his-redirect").onclick = function () {
+        // location.href = "phys-history.html";
+    };
+
+    document.getElementById("diet-his-redirect").onclick = function () {
+        // location.href = "diet-history.html";
+    };
 }
 redirect();
 
