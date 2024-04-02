26f44525ad6defab5d93a103eb86e8bdcf4ca6c8
Abhi Bagai <abagai@my.bcit.ca>
2024-04-02 11:05:46 -0700

    add saved html

--- scripts/each_workout.js
+++ scripts/each_workout.js
@@ -1,21 +1,3 @@
-function writeWorkouts() {
-    //define a variable for the collection you want to create in Firestore to populate data
-    var workoutRef = db.collection("workouts");
-
-    workoutRef.add({
-        workoutcode: "WOUB01",
-        name: "Uper Body 1.1", //replace with your own city?
-        level: "easy",
-        details: "Upper body strength training",
-        length: 25,          //number value
-        // hike_time: 60,       //number value
-
-        last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
-    });
-}
-
-// writeWorkouts()
-
 function displayWorkoutInfo() {
     let params = new URL(window.location.href); //get URL of search bar
     let ID = params.searchParams.get("docID"); //get value for key "id"
@@ -31,10 +13,23 @@ function displayWorkoutInfo() {
             workoutCode = thisworkout.workoutcode;
             workoutDetails = thisworkout.details;
             workoutTitle = thisworkout.title;
+            workoutLength = thisworkout.length;
             console.log(workoutCode);
+            console.log(workoutDetails);
+            console.log(workoutTitle);
+            console.log(thisworkout);
+            let woExcercises1 = thisworkout.excercises
+            // console.log(woExcercises1)
+            
+
+
+            
 
             // only populate title, and image
             document.getElementById("workoutName").innerHTML = workoutTitle;
+            document.getElementById("workoutDetails").innerHTML = workoutDetails;
+            document.getElementById("length").innerHTML = workoutLength + " min";
+            document.getElementById("excercises").innerHTML = woExcercises1;
             let imgEvent = document.querySelector(".workout-img");
             imgEvent.src = "../images/" + workoutCode + ".jpg";
         });
@@ -51,65 +46,65 @@ displayWorkoutInfo();
 //     window.location.href = 'review.html';
 // }
 
-function populateReviews() {
-    // console.log("test");
-    let DetailsTemplate = document.getElementById("DetailsTemplate");
-    // let hikeCardGroup = document.getElementById("reviewCardGroup");
+// function populateReviews() {
+//     // console.log("test");
+//     let DetailsTemplate = document.getElementById("DetailsTemplate");
+//     // let hikeCardGroup = document.getElementById("reviewCardGroup");
 
-    let params = new URL(window.location.href); // Get the URL from the search bar
-    let workoutID = params.searchParams.get("docID");
-    console.log(workoutID)
+//     let params = new URL(window.location.href); // Get the URL from the search bar
+//     let workoutID = params.searchParams.get("docID");
+//     console.log(workoutID)
 
 
-    // Double-check: is your collection called "Reviews" or "reviews"?
-    db.collection("workouts")
-        // .where("workoutID", "==", hikeID)
-        .doc(workoutID)
-        .get()
-        .then((allworkoutDetails) => {
-            // reviews = allReviews.docs;
-            console.log(reviews);
-            reviews.forEach((doc) => {
-                var title = doc.data().title;
-                var level = doc.data().level;
-                var season = doc.data().season;
-                var description = doc.data().description;
-                var flooded = doc.data().flooded;
-                var scrambled = doc.data().scrambled;
-                var time = doc.data().timestamp.toDate();
-                var rating = doc.data().rating; // Get the rating value
-                console.log(rating)
+//     // Double-check: is your collection called "Reviews" or "reviews"?
+//     db.collection("workouts")
+//         // .where("workoutID", "==", hikeID)
+//         .doc(workoutID)
+//         .get()
+//         .then((allReviews) => {
+//             // reviews = allReviews.docs;
+//             console.log(reviews);
+//             reviews.forEach((doc) => {
+//                 var title = doc.data().title;
+//                 var level = doc.data().level;
+//                 var season = doc.data().season;
+//                 var description = doc.data().description;
+//                 var flooded = doc.data().flooded;
+//                 var scrambled = doc.data().scrambled;
+//                 var time = doc.data().timestamp.toDate();
+//                 var rating = doc.data().rating; // Get the rating value
+//                 console.log(rating)
 
-                console.log(time);
+//                 console.log(time);
 
-                let reviewCard = hikeCardTemplate.content.cloneNode(true);
-                reviewCard.querySelector(".title").innerHTML = title;
-                reviewCard.querySelector(".time").innerHTML = new Date(
-                    time
-                ).toLocaleString();
-                reviewCard.querySelector(".level").innerHTML = `Level: ${level}`;
-                reviewCard.querySelector(".season").innerHTML = `Season: ${season}`;
-                reviewCard.querySelector(".scrambled").innerHTML = `Scrambled: ${scrambled}`;
-                reviewCard.querySelector(".flooded").innerHTML = `Flooded: ${flooded}`;
-                reviewCard.querySelector(".description").innerHTML = `Description: ${description}`;
+//                 let reviewCard = hikeCardTemplate.content.cloneNode(true);
+//                 reviewCard.querySelector(".title").innerHTML = title;
+//                 reviewCard.querySelector(".time").innerHTML = new Date(
+//                     time
+//                 ).toLocaleString();
+//                 reviewCard.querySelector(".level").innerHTML = `Level: ${level}`;
+//                 reviewCard.querySelector(".season").innerHTML = `Season: ${season}`;
+//                 reviewCard.querySelector(".scrambled").innerHTML = `Scrambled: ${scrambled}`;
+//                 reviewCard.querySelector(".flooded").innerHTML = `Flooded: ${flooded}`;
+//                 reviewCard.querySelector(".description").innerHTML = `Description: ${description}`;
 
-                // Populate the star rating based on the rating value
+//                 // Populate the star rating based on the rating value
 
-                // Initialize an empty string to store the star rating HTML
-                let starRating = "";
-                // This loop runs from i=0 to i<rating, where 'rating' is a variable holding the rating value.
-                for (let i = 0; i < rating; i++) {
-                    starRating += '<span class="material-icons">star</span>';
-                }
-                // After the first loop, this second loop runs from i=rating to i<5.
-                for (let i = rating; i < 5; i++) {
-                    starRating += '<span class="material-icons">star_outline</span>';
-                }
-                reviewCard.querySelector(".star-rating").innerHTML = starRating;
+//                 // Initialize an empty string to store the star rating HTML
+//                 let starRating = "";
+//                 // This loop runs from i=0 to i<rating, where 'rating' is a variable holding the rating value.
+//                 for (let i = 0; i < rating; i++) {
+//                     starRating += '<span class="material-icons">star</span>';
+//                 }
+//                 // After the first loop, this second loop runs from i=rating to i<5.
+//                 for (let i = rating; i < 5; i++) {
+//                     starRating += '<span class="material-icons">star_outline</span>';
+//                 }
+//                 reviewCard.querySelector(".star-rating").innerHTML = starRating;
 
-                hikeCardGroup.appendChild(reviewCard);
-            });
-        });
-}
+//                 hikeCardGroup.appendChild(reviewCard);
+//             });
+//         });
+// }
 
-populateReviews();
+// populateReviews();
